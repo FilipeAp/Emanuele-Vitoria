a52506cc59d8eb2958dfd22bcab583d5cab9cad6
Filipe <apolinariofilipe643@gmail.com>
2026-05-27 23:39:20 -0300

    Header feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import LogoEmanuele from './assets/images/LogoEmanuele.png'
 
 function App() {
   
   return (
-    <>
-      <header></header>
-    </>
+    <div>
+      <header>
+        <img src={LogoEmanuele} alt="Logo Emanuele Vitória" />
+        <ul>
+          <li><a href="">Procedimentos</a></li>
+          <li><a href="">Resultados</a></li>
+          <li><a href="">Sobre mim</a></li>
+          <li><a href="">Endereço & Contato</a></li>
+          <li><a href="">Endereço & Contato</a></li>
+        </ul>
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+
+      </footer>
+    </div>
   )
 }
 
