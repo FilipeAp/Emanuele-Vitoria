7416d8a64082e5f4296fcc172eb119804041cd2f
Filipe <apolinariofilipe643@gmail.com>
2026-05-28 02:26:26 -0300

    Primeira section finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-import LogoEmanuele from './assets/images/LogoEmanuele.png'
+import logoEmanuele from './assets/images/logoEmanuele.png'
+import iconCoracao from '../public/iconCoracao.png'
+import iconCalendario from '../public/iconCalendario.png'
+import imagemHome from './assets/images/imagemHome.png'
 
 function App() {
   
   return (
-    <div>
+    <div className="body2">
       <header>
-        <img src={LogoEmanuele} alt="Logo Emanuele Vitória" />
+        <img src={logoEmanuele} alt="Logo Emanuele Vitória" />
         <ul>
           <li><a href="">Procedimentos</a></li>
           <li><a href="">Resultados</a></li>
@@ -16,7 +19,18 @@ function App() {
         </ul>
       </header>
       <main>
-        
+        <section className="Inicio">
+          <div className="TextosInicio">
+            <div className="SeuMomento">
+              <img src={iconCoracao} alt="Icone de um coração"/>
+              <p>Seu momento seu cuidado</p>
+            </div>
+            <h1>Beleza, cuidado e leveza <span className="TextoRoxo">em um só lugar.</span></h1>
+            <p className="ParagrafoInicio">Um espaço pensado para você relaxar, se cuidar e sair diferente de como entrou.</p>
+            <a href="http://wa.me/11986507900" target="_blank" rel="noopener noreferrer"><img src={iconCalendario} alt="Icone de um calendário" />Agendar avaliação</a>
+          </div>
+        </section>
+        <section></section>
       </main>
       <footer>
 
